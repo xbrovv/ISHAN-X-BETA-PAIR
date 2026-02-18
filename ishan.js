07887616ac145ffe4f42e83e8d3c994c1bf9fe9c
xbrovv <ishanxbro847@gmail.com>
2026-02-18 20:33:56 +0530

    Update and rename arslan.js to ishan.js

--- ishan.js
+++ ishan.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star Arslan-MD ⭐ 
+Don't Forget To Give Star ISHAN-X MD ⭐ 
 
  Server running on http://localhost:` + PORT)
 })
